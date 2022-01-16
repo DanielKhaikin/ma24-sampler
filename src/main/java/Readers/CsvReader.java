4bb53af4f2fe8e00ae5e325ab65e2a464ffe5cc6
DanielKhaikin <daniel2003khaikin@gmail.com>
2022-01-16 16:35:29 +0200

    added cons getts setts and readData

--- src/main/java/Readers/CsvReader.java
+++ src/main/java/Readers/CsvReader.java
@@ -1,2 +1,26 @@
-package Readers;public class CsvReader {
+package Readers;
+
+import java.io.File;
+import java.util.Set;
+
+public class CsvReader implements Reader{
+
+    private File file;
+
+    public CsvReader(File file) {
+        this.file = file;
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    @Override
+    public Set readData() {
+        return null;
+    }
 }
