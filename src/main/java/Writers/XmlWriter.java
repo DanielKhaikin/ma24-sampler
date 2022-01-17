09da608552a21606f68ee0f04293fc5721ee860e
DanielKhaikin <daniel2003khaikin@gmail.com>
2022-01-17 09:38:53 +0200

    created class

---
+++ src/main/java/Writers/XmlWriter.java
@@ -0,0 +1,25 @@
+package Writers;
+
+import java.io.File;
+import java.util.LinkedList;
+
+public class XmlWriter implements Writer{
+    private File file;
+
+    public XmlWriter(File file) {
+        this.file = file;
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    @Override
+    public void writeData(LinkedList list) {
+
+    }
+}
