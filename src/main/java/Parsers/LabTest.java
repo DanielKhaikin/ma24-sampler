64f81088ad142af1a1980bbfea7064b60b01ea6f
DanielKhaikin <daniel2003khaikin@gmail.com>
2022-01-17 08:21:56 +0200

    finished class with cons, getter and setter

--- src/main/java/Parsers/LabTest.java
+++ src/main/java/Parsers/LabTest.java
@@ -1,2 +1,147 @@
-package Parsers;public class LabTest {
+package Parsers;
+
+public class LabTest {
+    private String IDNum;
+    private String IDType;
+    private String firstName;
+    private String lastName;
+    private String resultDate;
+    private String birthDate;
+    private String labCode;
+    private String stickerNumber;
+    private String resultTestCorona;
+    private String variant;
+    private String testType;
+    private String joinDate;
+    private String healthCareID;
+    private String healthCareName;
+
+    public LabTest(String IDNum, String IDType, String firstName, String lastName, String resultDate, String birthDate, String labCode, String stickerNumber, String resultTestCorona, String variant, String testType, String joinDate, String healthCareID, String healthCareName) {
+        this.IDNum = IDNum;
+        this.IDType = IDType;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.resultDate = resultDate;
+        this.birthDate = birthDate;
+        this.labCode = labCode;
+        this.stickerNumber = stickerNumber;
+        this.resultTestCorona = resultTestCorona;
+        this.variant = variant;
+        this.testType = testType;
+        this.joinDate = joinDate;
+        this.healthCareID = healthCareID;
+        this.healthCareName = healthCareName;
+    }
+
+    public String getIDNum() {
+        return IDNum;
+    }
+
+    public void setIDNum(String IDNum) {
+        this.IDNum = IDNum;
+    }
+
+    public String getIDType() {
+        return IDType;
+    }
+
+    public void setIDType(String IDType) {
+        this.IDType = IDType;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getResultDate() {
+        return resultDate;
+    }
+
+    public void setResultDate(String resultDate) {
+        this.resultDate = resultDate;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getLabCode() {
+        return labCode;
+    }
+
+    public void setLabCode(String labCode) {
+        this.labCode = labCode;
+    }
+
+    public String getStickerNumber() {
+        return stickerNumber;
+    }
+
+    public void setStickerNumber(String stickerNumber) {
+        this.stickerNumber = stickerNumber;
+    }
+
+    public String getResultTestCorona() {
+        return resultTestCorona;
+    }
+
+    public void setResultTestCorona(String resultTestCorona) {
+        this.resultTestCorona = resultTestCorona;
+    }
+
+    public String getVariant() {
+        return variant;
+    }
+
+    public void setVariant(String variant) {
+        this.variant = variant;
+    }
+
+    public String getTestType() {
+        return testType;
+    }
+
+    public void setTestType(String testType) {
+        this.testType = testType;
+    }
+
+    public String getJoinDate() {
+        return joinDate;
+    }
+
+    public void setJoinDate(String joinDate) {
+        this.joinDate = joinDate;
+    }
+
+    public String getHealthCareID() {
+        return healthCareID;
+    }
+
+    public void setHealthCareID(String healthCareID) {
+        this.healthCareID = healthCareID;
+    }
+
+    public String getHealthCareName() {
+        return healthCareName;
+    }
+
+    public void setHealthCareName(String healthCareName) {
+        this.healthCareName = healthCareName;
+    }
 }
