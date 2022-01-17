977e454014119154226fec71b304f3230b68cc07
DanielKhaikin <daniel2003khaikin@gmail.com>
2022-01-17 08:43:30 +0200

    finished transform class

--- src/main/java/Transforms/TransformB.java
+++ src/main/java/Transforms/TransformB.java
@@ -1,2 +1,34 @@
-package Transforms;public class TransformB {
+package Transforms;
+
+import health_care_provider.HealthCareInfoProvider;
+import health_care_provider.errors.InvalidIdException;
+import health_care_provider.models.PersonInsured;
+
+public class TransformB {
+
+    public PersonInsured start(String IDNum, String IDType){
+        HealthCareInfoProvider healthCareInfoProvider = new HealthCareInfoProvider();
+        PersonInsured personInsured = null;
+        try {
+            personInsured = healthCareInfoProvider.fetchInfo(Integer.parseInt(IDNum),Integer.parseInt(IDType));
+        } catch (InvalidIdException e) {
+            e.printStackTrace();
+        }
+        return personInsured;
+    }
+
+    public String getJoin(String IDNum, String IDType){
+        PersonInsured personInsured = start(IDNum,IDType);
+        return String.valueOf(personInsured.getJoinDate());
+    }
+
+    public String getHealthID(String IDNum, String IDType){
+        PersonInsured personInsured = start(IDNum,IDType);
+        return String.valueOf(personInsured.getHealthCareId());
+    }
+
+    public String getHealthName(String IDNum, String IDType){
+        PersonInsured personInsured = start(IDNum,IDType);
+        return personInsured.getHealthCareName();
+    }
 }
