a3324bb3dac2e8838d0199d392ff4aeaa87c1b72
DanielKhaikin <daniel2003khaikin@gmail.com>
2022-01-17 18:20:01 +0200

    everything workes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,14 @@
 
+import DBToDB.CsvToXml;
+
 import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
         File csvfile = new File("C:\\Users\\Daniel\\HaDogemHaMatmod\\src\\main\\resources\\LabTests.csv");
-        File xmlFile = new File("C:\\Users\\Daniel\\HadogemHamatmidJson\\file3.xml");
+        File xmlFile = new File("C:\\Users\\Daniel\\LABTESTS\\file1.xml");
+        CsvToXml csvToXml = new CsvToXml(csvfile, xmlFile);
+        csvToXml.execute();
         System.out.println("finished");
     }
 }
