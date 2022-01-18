81e9b6cf307432ef94cd18f0cb774220a10cb29e
DanielKhaikin <daniel2003khaikin@gmail.com>
2022-01-18 15:37:54 +0200

    copied evertyhing I could, now create parser

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,11 +5,7 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-        File csvfile1 = new File("C:\\Users\\Daniel\\HaDogemHaMatmod\\src\\main\\resources\\LabTests.csv");
-        File csvfile2 = new File("C:\\Users\\Daniel\\HaDogemHaMatmod\\src\\main\\resources\\MadaReports.csv");
-        File jsonFile = new File("C:\\Users\\Daniel\\POSITIVE_CORONA_PEOPLE\\file1.json");
-        CrossedDataToJson crossedDataToJson= new CrossedDataToJson(csvfile2, csvfile1, jsonFile);
-        crossedDataToJson.execute();
+        File csvfile = new File("C:\\Users\\Daniel\\HaDogemHaMatmod\\src\\main\\resources\\Serology.csv");
         System.out.println("finished");
     }
 }
---
+++ src/main/java/Readers/CsvReaderD.java
@@ -0,0 +1,68 @@
+package Readers;
+
+import Parsers.LabTest;
+import Transforms.TransformB;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashSet;
+import java.util.Set;
+
+public class CsvReaderD implements Reader{
+    private File file;
+
+    final static String DELIMITER = ",";
+
+    public CsvReaderD(File file) {
+        this.file = file;
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+    @Override
+    public Set readData() {
+        HashSet<LabTest> labTests = new HashSet<>();
+        try {
+            BufferedReader br = Files.newBufferedReader(Path.of(this.file.getAbsolutePath()));
+            String line = br.readLine();
+            int count = 1;
+            while ((line = br.readLine()) != null){
+                String[] record = line.split(DELIMITER);
+                LabTest labTest = recordToLabTests(record);
+                System.out.println("read " + count);
+                count++;
+                labTests.add(labTest);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return labTests;
+    }
+
+    public LabTest recordToLabTests(String[] record){
+        String IDNum = record[0];
+        String IDType = record[1];
+        String  firstName= record[2];
+        String lastName = record[3];
+        String resultDate = record[4];
+        String birthDate = record[5];
+        String labCode = record[6];
+        String stickerNumber = record[7];
+        String resultTestCorona = record[8];
+        String variant = record[9];
+        String testType = record[10];
+        TransformB transformB = new TransformB();
+        String joinDate = transformB.getJoin(IDNum,IDType);
+        String healthCareID = transformB.getHealthID(IDNum,IDType);
+        String healthCareName = transformB.getHealthName(IDNum,IDType);
+        return new LabTest(IDNum, IDType, firstName, lastName, resultDate, birthDate, labCode, stickerNumber, resultTestCorona, variant, testType, joinDate, healthCareID, healthCareName);
+    }
+}
