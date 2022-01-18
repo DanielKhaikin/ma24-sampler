8d22d5529d7adda17833a99ac455c17bb90bdef8
DanielKhaikin <daniel2003khaikin@gmail.com>
2022-01-18 09:21:45 +0200

    finished class

--- src/main/java/Transforms/CrossDataFromCsv.java
+++ src/main/java/Transforms/CrossDataFromCsv.java
@@ -1,2 +1,22 @@
-package Transforms;public class CrossDataFromCsv {
+package Transforms;
+
+import Parsers.CrossedData;
+import Parsers.LabTest;
+import Parsers.MadaReport;
+
+import java.util.HashSet;
+
+public class CrossDataFromCsv {
+    private HashSet<CrossedData> crossData(HashSet<LabTest> labTests, HashSet<MadaReport> madaReports){
+        HashSet<CrossedData> crossedData = new HashSet<>();
+        for (LabTest labTest : labTests){
+            for (MadaReport madaReport : madaReports){
+                if ((madaReport.getIDNum() == labTest.getIDNum()) && (madaReport.getIDType() == labTest.getIDType())){
+                    crossedData.add(new CrossedData(madaReport.getIDNum(), madaReport.getIDType(), madaReport.getFirstName(), madaReport.getLastName(), madaReport.getCity(), madaReport.getStreet(), madaReport.getBuildingNumber(), madaReport.getBarcode(), labTest.getBirthDate(), labTest.getLabCode(), labTest.getResultDate(), madaReport.getTakeDate(), labTest.getStickerNumber(), labTest.getResultTestCorona(), labTest.getVariant(), labTest.getTestType()));
+                    madaReports.remove(madaReport);
+                }
+            }
+        }
+        return crossedData;
+    }
 }
