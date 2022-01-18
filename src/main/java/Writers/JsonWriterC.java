73aa0fdc4df4de226947c6409df0756001d6be75
DanielKhaikin <daniel2003khaikin@gmail.com>
2022-01-18 09:30:01 +0200

    finished class

--- src/main/java/Writers/JsonWriterC.java
+++ src/main/java/Writers/JsonWriterC.java
@@ -1,11 +1,30 @@
 package Writers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.LinkedList;
 
 public class JsonWriterC implements Writer{
 
+    private File file;
+
+    public JsonWriterC(File file) {
+        this.file = file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
     @Override
     public void writeData(LinkedList t) {
-        
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            objectMapper.writeValue(file, t);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
