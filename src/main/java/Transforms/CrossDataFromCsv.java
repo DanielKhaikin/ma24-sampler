a1a15e08378733cf3b07abdb8e0e15a440eed3bd
DanielKhaikin <daniel2003khaikin@gmail.com>
2022-01-18 14:36:15 +0200

    I think everything should work, but something is wron

--- src/main/java/Transforms/CrossDataFromCsv.java
+++ src/main/java/Transforms/CrossDataFromCsv.java
@@ -11,8 +11,9 @@ public class CrossDataFromCsv {
         HashSet<CrossedData> crossedData = new HashSet<>();
         for (LabTest labTest : labTests){
             for (MadaReport madaReport : madaReports){
-                if (madaReport.getIDNum() == labTest.getIDNum()){
-                    crossedData.add(new CrossedData(madaReport.getIDNum(), madaReport.getIDType(), madaReport.getFirstName(), madaReport.getLastName(), madaReport.getCity(), madaReport.getStreet(), madaReport.getBuildingNumber(), madaReport.getBarcode(), labTest.getBirthDate(), labTest.getLabCode(), labTest.getResultDate(), madaReport.getTakeDate(), labTest.getStickerNumber(), labTest.getResultTestCorona(), labTest.getVariant(), labTest.getTestType()));
+                if (madaReport.getIDNum().equals(labTest.getIDNum())){
+                    CrossedData newCrossedData = new CrossedData(madaReport.getIDNum(), madaReport.getIDType(), madaReport.getFirstName(), madaReport.getLastName(), madaReport.getCity(), madaReport.getStreet(), madaReport.getBuildingNumber(), madaReport.getBarcode(), labTest.getBirthDate(), labTest.getLabCode(), labTest.getResultDate(), madaReport.getTakeDate(), labTest.getStickerNumber(), labTest.getResultTestCorona(), labTest.getVariant(), labTest.getTestType());
+                    crossedData.add(newCrossedData);
                 }
             }
         }
