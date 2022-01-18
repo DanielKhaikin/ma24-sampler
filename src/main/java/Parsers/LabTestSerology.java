bdeb1d2fa534c7f193d0d79bea63df5f82b12ed7
DanielKhaikin <daniel2003khaikin@gmail.com>
2022-01-18 15:43:22 +0200

    finished parser LabTestSerolgy

--- src/main/java/Parsers/LabTestSerology.java
+++ src/main/java/Parsers/LabTestSerology.java
@@ -1,4 +1,27 @@
 package Parsers;
 
 public class LabTestSerology {
+    private String IDNum;
+    private String IDType;
+    private String firstName;
+    private String lastName;
+    private String resultDate;
+    private String birthDate;
+    private String labCode;
+    private String stickerNumber;
+    private String antidotes;
+    private String kitNumber;
+
+    public LabTestSerology(String IDNum, String IDType, String firstName, String lastName, String resultDate, String birthDate, String labCode, String stickerNumber, String antidotes, String kitNumber) {
+        this.IDNum = IDNum;
+        this.IDType = IDType;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.resultDate = resultDate;
+        this.birthDate = birthDate;
+        this.labCode = labCode;
+        this.stickerNumber = stickerNumber;
+        this.antidotes = antidotes;
+        this.kitNumber = kitNumber;
+    }
 }
